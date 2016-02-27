d20cafc821c59215b37134ffdea28dc33bbd6e39
Tommy Lee <tommy.lee@frshlns.com>
2016-02-27 15:51:16 -0500

    Change Ports

--- server/app.js
+++ server/app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -24,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/logins', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/routes/users.js
+++ server/routes/users.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+  //res.render('colors', { title: 'Express mann' });
 });
 
 module.exports = router;
