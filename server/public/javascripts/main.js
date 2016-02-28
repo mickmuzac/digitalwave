453567cfa6ea90da4628acf478a50380aba42bb9
Mick Muzac <mmuzac23@gmail.com>
2016-02-28 10:39:48 -0500

    Better camera access error detection

--- server/public/javascripts/main.js
+++ server/public/javascripts/main.js
@@ -21,21 +21,6 @@ canvas.width = 480;
 canvas.height = 360;
 
 var button = document.querySelector('button');
-window.setInterval(function() {
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-  canvas.getContext('2d').
-    drawImage(video, 0, 0, canvas.width, canvas.height);
-
-  var vibrant = new Vibrant(canvas);
-  var swatches = vibrant.swatches();
-  
-  if(swatches["Vibrant"]){
-    document.querySelector("body").style.background = swatches["Vibrant"].getHex();
-  }
-
-}, 1/fps * 1000);
-
 var constraints = {
   audio: false,
   video: true
@@ -44,10 +29,27 @@ var constraints = {
 function successCallback(stream) {
   window.stream = stream; // make stream available to browser console
   video.srcObject = stream;
+
+  window.setInterval(function() {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  canvas.getContext('2d').
+    drawImage(video, 0, 0, canvas.width, canvas.height);
+
+  var vibrant = new Vibrant(canvas);
+  var swatches = vibrant.swatches();
+
+  if(swatches["Vibrant"]){
+    document.querySelector("body").style.background = swatches["Vibrant"].getHex();
+  }
+
+  }, 1/fps * 1000);
+
 }
 
 function errorCallback(error) {
   console.log('navigator.getUserMedia error: ', error);
 }
 
-navigator.getUserMedia(constraints, successCallback, errorCallback);
+if(navigator.getUserMedia) navigator.getUserMedia(constraints, successCallback, errorCallback);
+else errorCallback(new Error("navigator.getUserMedia not found"));
