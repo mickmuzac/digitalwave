510e1c0e473a40b9d655e2a07e2e1326c4fd11d5
Tommy Lee <tommy.lee@frshlns.com>
2016-02-28 13:28:58 -0500

    Add offset

--- server/public/javascripts/color.js
+++ server/public/javascripts/color.js
@@ -7,15 +7,15 @@ socket.on('both', function (data) {
   var tempArr = data.split(",");
   var colorIndex = Number(tempArr[0]);
   var schemeIndex = Number(tempArr[1]);
-  schemeIndex += localStorage.getItem("radialSection");
+  //Calculating offset
+  colorIndex += parseInt(localStorage.getItem("radialSection"));
   new_color = getColor(schemeIndex, colorIndex);
   $('#main_page').css('background-color', new_color);
 });
 
 function getColor(scheme, colorIndex) {
   var team_colors = JSON.parse(localStorage.getItem('teamColors'));
-  console.log(team_colors);
-  var max_width = team_colors[colorIndex].length;
-  var new_color = team_colors[colorIndex][scheme % max_width];
+  var max_width = team_colors[scheme].length;
+  var new_color = team_colors[scheme][colorIndex % max_width];
   return new_color;
 }
