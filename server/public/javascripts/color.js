67cd4747d2eca0cb5a66f0daf96014974745e9eb
Tommy Lee <tommy.lee@frshlns.com>
2016-02-28 13:04:50 -0500

    Integrate new socket io changes

--- server/public/javascripts/color.js
+++ server/public/javascripts/color.js
@@ -2,20 +2,20 @@ document.getElementById('main_page').style.transition = 'background .5s';
 document.getElementById('main_page').style.WebKitTransition = 'background .5s';
 
 var socket = io.connect();
-
+var team_colors = JSON.parse(localStorage.getItem('teamColors'));
 socket.on('both', function (data) {
   var tempArr = data.split(",");
   var colorIndex = Number(tempArr[0]);
   var schemeIndex = Number(tempArr[1]);
-
   schemeIndex += localStorage.getItem("radialSection");
-  new_color = getColor(scheme, schemeIndex);
+  new_color = getColor(schemeIndex, colorIndex);
   $('#main_page').css('background-color', new_color);
 });
 
-function getColor(scheme, index) {
+function getColor(scheme, colorIndex) {
   var team_colors = JSON.parse(localStorage.getItem('teamColors'));
-  var max_width = team_colors[scheme].length;
-  var new_color = team_colors[scheme][index % max_width];
+  console.log(team_colors);
+  var max_width = team_colors[colorIndex].length;
+  var new_color = team_colors[colorIndex][scheme % max_width];
   return new_color;
 }
