ca363f9ecc9f11911f6b28c004de96488b7e2287
Mick Muzac <mmuzac23@gmail.com>
2016-02-28 10:16:08 -0500

    Added support for CSS background color and transitions #15

--- server/public/javascripts/main.js
+++ server/public/javascripts/main.js
@@ -8,9 +8,13 @@
 
 'use strict';
 
-var fps = 2;
+var fps = 0.5;
 
 // Put variables in global scope to make them available to the browser console.
+
+document.querySelector('body').style.transition = 'background 1s';
+document.querySelector('body').style.WebKitTransition = 'background 1s';
+
 var video = document.querySelector('video');
 var canvas = window.canvas = document.querySelector('canvas');
 canvas.width = 480;
@@ -22,6 +26,14 @@ window.setInterval(function() {
   canvas.height = video.videoHeight;
   canvas.getContext('2d').
     drawImage(video, 0, 0, canvas.width, canvas.height);
+
+  var vibrant = new Vibrant(canvas);
+  var swatches = vibrant.swatches();
+  
+  if(swatches["Vibrant"]){
+    document.querySelector("body").style.background = swatches["Vibrant"].getHex();
+  }
+
 }, 1/fps * 1000);
 
 var constraints = {
