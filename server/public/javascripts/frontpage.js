2352c33b7f3b6e1b8ea8ab82674220838af22af3
kibblebits <tfindling@knights.ucf.edu>
2016-02-27 21:43:32 -0500

    Added seat section row submit button.
    Added seat section row submit button.

---
+++ server/public/javascripts/frontpage.js
@@ -0,0 +1,7 @@
+submitseat.onclick = storeSeatNumber;
+
+function storeSeatNumber(){
+	localStorage.setItem("seatnumber", seatnumber.value);
+	localStorage.setItem("rownumber", rownumber.value);
+	localStorage.setItem("sectionnumber", sectionnumber.value);
+}
