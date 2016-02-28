f72aea1d1068246cb29f6adb6691804a87064cd1
Mick Muzac <mick.muzac.ctr@adlnet.gov>
2016-02-28 12:39:31 -0500

    Resolved socket.io issues

--- server/bin/colorSocket.js
+++ server/bin/colorSocket.js
@@ -1,9 +1,13 @@
 var totalColors = 18;
-var colorsPerSec = 3;
+var totalSchemes = 2;
 
-var index = 0;
+var colorsPerSec = 1; //Number of color changes per second
+var timeSchemeSwitch = 30; //Seconds between scheme changes
+
+var index = 0; //color index
+var scheme = 0; //scheme index
+
 var io;
-var socket2;
 
 exports.start = function(server){
   io = require('socket.io')(server);
@@ -11,16 +15,20 @@ exports.start = function(server){
   // Pass in a connection
   io.on('connection', function (socket) {
     console.log("Connected!");
-    socket.emit('c', index);
-
-    socket2 = socket;
-    global.setInterval(increment, 1/colorsPerSec * 1000);
+    socket.emit('both', `${index},${scheme}`);
   });
 
+  global.setInterval(increment, 1/colorsPerSec * 1000);
+  global.setInterval(incrementScheme, timeSchemeSwitch * 1000);
+}
+
+function incrementScheme(){
+  scheme = (scheme + 1) % totalSchemes;
+  io.emit('scheme', scheme);
 }
 
 function increment(){
     console.log("Emit!");
     index = (index + 1) % totalColors;
-    socket2.emit('c', index);
+    io.emit('color', index);
 }
--- server/public/javascripts/color.js
+++ server/public/javascripts/color.js
@@ -1,7 +1,23 @@
-var socket = io.connect("http://localhost:4001/");
+var socket = io.connect();
 
-socket.on('colorchange', function (data) {
-  new_color = data.color;
-  $('#main_page').css('background-color', new_color);
+function setColor(colorIndex){
+  console.log("Set color", colorIndex);
+  // $('#main_page').css('background-color', /* To-do */);
+}
+
+function setScheme(schemeIndex){
+  console.log("Set scheme", schemeIndex);
+  // $('#main_page').css('background-color', /* To-do */);
+}
+
+socket.on('color', setColor);
+socket.on('scheme', setScheme);
+
+socket.on('both', function (data) {
+  var tempArr = data.split(",");
+  var colorIndex = Number(tempArr[0]);
+  var schemeIndex = Number(tempArr[1]);
+
+  setScheme(schemeIndex);
+  setColor(colorIndex);
 });
-
