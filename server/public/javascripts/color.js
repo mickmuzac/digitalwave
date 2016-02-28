8f4e5e96f0601203b69c7808148dc58a77c6f379
Tommy Lee <tommy.lee@frshlns.com>
2016-02-27 19:57:20 -0500

    Remove debugging color statement

--- server/public/javascripts/color.js
+++ server/public/javascripts/color.js
@@ -1,8 +1,8 @@
 var socket = io.connect("http://localhost:4001/");
 
+// Detect a color change, and change the background on it.
 socket.on('colorchange', function (data) {
   new_color = data.color;
-  console.log(new_color);
   $('#main_page').css('background-color', new_color);
 });
 
